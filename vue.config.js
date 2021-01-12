d90a9da71c2e57455095ef430bf54df1b136c441
Dmitry Lartsev <dmitry.lartsev@noveogroup.com>
2021-01-12 17:09:47 +0300

    Edit env

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const publicPath = process.env.NODE_ENV === 'production' ? process.env.VUE_APP_REPO_NAME : '/'
 
 module.exports = {
+  publicPath,
   chainWebpack: config => {
     config.module
       .rule('svg-sprite')
